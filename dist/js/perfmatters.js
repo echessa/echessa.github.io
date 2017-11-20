cb966c1dd10a6698effddcb7fb12615bbf6f1789
Joyce Echessa <jokhessa@yahoo.com>
2017-11-20 12:21:53 +0300

    Add temporary 'dist' folder

---
+++ dist/js/perfmatters.js
@@ -0,0 +1,1 @@
+function logCRP(){var t=window.performance.timing,dcl=t.domContentLoadedEventStart-t.domLoading,complete=t.domComplete-t.domLoading;document.getElementById("crp-stats").textContent="DCL: "+dcl+"ms, onload: "+complete+"ms"}window.addEventListener("load",function(event){logCRP()});
